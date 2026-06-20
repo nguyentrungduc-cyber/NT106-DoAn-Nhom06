1f92ce01931ac33221e932607c421c4e60b7c779
nguyentrungduc <122720104+nguyentrungduc-cyber@users.noreply.github.com>
2026-06-20 22:31:17 +0700

    Commit khi merge Dev

---
+++ SecureChat.Server/Migrations/20260619062916_AutoFixMissingColumns.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecureChat.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoFixMissingColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_seen_utc",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_seen_utc",
+                table: "Users");
+        }
+    }
+}
